77451089507845ecb072bed6bc21a8197de06331
Jobie Winser <winserjm@cardiff.ac.uk>
2020-03-29 13:57:36 +0100

    working with lists

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -4,34 +4,40 @@ import './App.css';
 
 function App() {
 
+    const numbers = ["item1","item2","item3","item4"];
+
+    function NumberList(props) {
+        const numbers = props.numbers;
+        const listItems = numbers.map((number) =>
+            <li>{number}</li>
+        );
+        return (
+            <ul>{listItems}</ul>
+        );
+    }
 
 
+    return (
 
-  let myNewP = document.createElement("h1");
-  myNewP.innerHTML = "This is a paragraph.";
+        <div className="App">
+            <NumberList numbers={numbers} />,
 
-
-  return (
-
-      <div className="App">
-        {myNewP.innerText}
-
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+        </div>
+    );
 }
 
 export default App;
